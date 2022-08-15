13e62b3593075fb2ff29e3cb4bf3311ec8234a16
shuangzhou <shuangzhou@wustl.edu>
2022-08-15 16:28:32 -0500

    add auto data type detection

--- src/protonDose_src/ProtonDose3D.cpp
+++ src/protonDose_src/ProtonDose3D.cpp
@@ -1,9 +1,9 @@
+#include <omp.h>
 #include "gauss2d.h"
+#include "matrix.h"
 #include "mex.h"
-using T = float;
-using vec = std::vector<T>;
-using host_vec = thrust::host_vector<T>;
 
+template<class T>
 void updata_Ai(T* gauss_para, T* idd_o_ptr, int Nz, int N_gaussian, int Ng)
 {
     // Ng : number of parameters in gaussian2d model , 4 or 6
@@ -21,18 +21,12 @@ void updata_Ai(T* gauss_para, T* idd_o_ptr, int Nz, int N_gaussian, int Ng)
 
 // disable matlab entry function
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-    float *X;
-    float *Y;
-    float *Z;
-    float *gauss_para;
-    float *bf_para;
-    X = (float*)mxGetPr(prhs[0]);
-    Y = (float*)mxGetPr(prhs[1]);
-    Z = (float*)mxGetPr(prhs[2]);
-
-    gauss_para = (float*)mxGetPr(prhs[3]);
-    bf_para = (float*)mxGetPr(prhs[4]);
-
+    mxClassID id0 = mxGetClassID(prhs[0]);
+    mxClassID id1 = mxGetClassID(prhs[1]);
+    mxClassID id2 = mxGetClassID(prhs[2]);
+    mxClassID id3 = mxGetClassID(prhs[2]);
+    mxClassID id4 = mxGetClassID(prhs[2]);
+    // common parts
     int N_gaussian = static_cast<int>(*mxGetPr(prhs[5]));
     int isGPU = static_cast<int>(*mxGetPr(prhs[6]));
 
@@ -47,25 +41,42 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
     int Nz = static_cast<int>(dim_Z[0]*dim_Z[1]);
     int N_gauss_para = static_cast<int>(dim_gauss_para[0]*dim_gauss_para[1]);
     int N_bf_para = static_cast<int>(dim_bf_para[0]*dim_bf_para[1]);
-
-    // gauss2d 
-    std::vector<float> X_vec(Nx), Y_vec(Ny);
-    std::copy(X, X + Nx, X_vec.begin());
-    std::copy(Y, Y + Ny, Y_vec.begin());
-    std::vector<float> gauss_para_vec(N_gauss_para);
-    std::copy(gauss_para, gauss_para + N_gauss_para, gauss_para_vec.begin());
-    // output 
-    const mwSize size[3]{ mwSize(Nx), mwSize(Ny), mwSize(Nz) };
-    plhs[0] = mxCreateNumericArray(3, size, mxSINGLE_CLASS, mxREAL);//mxDOUBLE_CLASS
-    float* dose3d_ptr{};
-    dose3d_ptr = (float*)mxGetPr(plhs[0]);
-    
-    // run calculation
-    vec idd_o(Nz);
-    // step 1 get IDD
-	BP::IDD_array_N( Z, idd_o.data(), bf_para, Nz, N_bf_para);
-    if((N_gauss_para == Nz*N_gaussian*6) | (N_gauss_para == Nz*N_gaussian*4))
+    if(!((N_gauss_para == Nz*N_gaussian*6) | (N_gauss_para == Nz*N_gaussian*4)))
     {
+        mexPrintf("N_gaussian(%d),Nz(%d) not match N_gauss_para(%d)!\n",N_gaussian,Nz,N_gauss_para);
+        mexErrMsgTxt("Size not match!\n");
+    }
+    mxClassID mex_type_class = mxSINGLE_CLASS;
+    if(id0 + id1 + id2 + id3 + id4 == 5*mxDOUBLE_CLASS)
+    {
+        using mex_type1 = double;
+        mex_type_class = mxDOUBLE_CLASS; 
+        mex_type1 *X;
+        mex_type1 *Y;
+        mex_type1 *Z;
+        mex_type1 *gauss_para;
+        mex_type1 *bf_para;
+        X = (mex_type1*)mxGetPr(prhs[0]);
+        Y = (mex_type1*)mxGetPr(prhs[1]);
+        Z = (mex_type1*)mxGetPr(prhs[2]);
+        gauss_para = (mex_type1*)mxGetPr(prhs[3]);
+        bf_para = (mex_type1*)mxGetPr(prhs[4]);
+
+        // gauss2d 
+        std::vector<mex_type1> X_vec(Nx), Y_vec(Ny);
+        std::vector<mex_type1> gauss_para_vec(N_gauss_para);
+        std::copy(X, X + Nx, X_vec.begin());
+        std::copy(Y, Y + Ny, Y_vec.begin());
+        std::copy(gauss_para, gauss_para + N_gauss_para, gauss_para_vec.begin());
+        // output 
+        const mwSize size[3]{ mwSize(Nx), mwSize(Ny), mwSize(Nz) };
+        plhs[0] = mxCreateNumericArray(3, size, mex_type_class, mxREAL);
+        mex_type1* dose3d_ptr{};
+        dose3d_ptr = (mex_type1*)mxGetPr(plhs[0]);
+        // run calculation
+        std::vector<mex_type1> idd_o(Nz);
+        // step 1 get IDD
+	    BP::IDD_array_N( Z, idd_o.data(), bf_para, Nz, N_bf_para);
         int Ng = N_gauss_para / (Nz * N_gaussian);
         // step 2 update weight Ai in Gauss2d model
 	    updata_Ai(gauss_para_vec.data(), idd_o.data(), Nz, N_gaussian, Ng);
@@ -79,8 +90,50 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
             Gauss2d::cuda_interface( X_vec,  Y_vec,  gauss_para_vec, dose3d_ptr, Nx, Ny, Nz, N_gauss_para, N_gaussian);// gpu version
         }
     }
-    else
+    else if(id0 + id1 + id2 + id3 + id4 == 5*mxSINGLE_CLASS)
     {
-        mexPrintf("N_gaussian(%d),Nz(%d) not match para size(%d)!\n",N_gaussian,Nz,N_gauss_para);
+        using mex_type2 = float;
+        mex_type_class = mxSINGLE_CLASS; 
+        mex_type2 *X;
+        mex_type2 *Y;
+        mex_type2 *Z;
+        mex_type2 *gauss_para;
+        mex_type2 *bf_para;
+        X = (mex_type2*)mxGetPr(prhs[0]);
+        Y = (mex_type2*)mxGetPr(prhs[1]);
+        Z = (mex_type2*)mxGetPr(prhs[2]);
+        gauss_para = (mex_type2*)mxGetPr(prhs[3]);
+        bf_para = (mex_type2*)mxGetPr(prhs[4]);
+
+        // gauss2d 
+        std::vector<mex_type2> X_vec(Nx), Y_vec(Ny);
+        std::vector<mex_type2> gauss_para_vec(N_gauss_para);
+        std::copy(X, X + Nx, X_vec.begin());
+        std::copy(Y, Y + Ny, Y_vec.begin());
+        std::copy(gauss_para, gauss_para + N_gauss_para, gauss_para_vec.begin());
+        // output 
+        const mwSize size[3]{ mwSize(Nx), mwSize(Ny), mwSize(Nz) };
+        plhs[0] = mxCreateNumericArray(3, size, mex_type_class, mxREAL);
+        mex_type2* dose3d_ptr{};
+        dose3d_ptr = (mex_type2*)mxGetPr(plhs[0]);
+        // run calculation
+        std::vector<mex_type2> idd_o(Nz);
+        // step 1 get IDD
+	    BP::IDD_array_N( Z, idd_o.data(), bf_para, Nz, N_bf_para);
+        int Ng = N_gauss_para / (Nz * N_gaussian);
+        // step 2 update weight Ai in Gauss2d model
+	    updata_Ai(gauss_para_vec.data(), idd_o.data(), Nz, N_gaussian, Ng);
+        // step 3 get 3D dose layer by layer
+        if(isGPU == 0)
+        {
+            Gauss2d::interface( X_vec, Y_vec, gauss_para_vec, dose3d_ptr, Nx, Ny, Nz, N_gauss_para, N_gaussian);// cpu version
+        }
+	    else
+        {
+            Gauss2d::cuda_interface( X_vec,  Y_vec,  gauss_para_vec, dose3d_ptr, Nx, Ny, Nz, N_gauss_para, N_gaussian);// gpu version
+        }
+    }
+    else{
+        mexErrMsgTxt("Inconsistency float point input data type!, Should be all double or single\n");
     }
 }
