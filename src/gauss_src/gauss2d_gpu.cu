2ff0b6fae6a84ea3bc41537b5535b79cad43e1cf
shuangzhou <shuangzhou@wustl.edu>
2022-08-15 16:27:44 -0500

    overload gpu kernel functions

--- src/gauss_src/gauss2d_gpu.cu
+++ src/gauss_src/gauss2d_gpu.cu
@@ -22,14 +22,14 @@
 // #include <filesystem>
 #include "gauss2d.h"
 
-using T = float;
-using host_vec = thrust::host_vector<float>;
-using device_vec = thrust::device_vector<float>;
 
-__constant__ const T const_1_PI = 0.318309886183791;
-__constant__ const T const_1_SQRT_2PI = 0.398942280401433;
-// 1/sqrt(2*pi), 1D gauss constant
-__constant__ const T const_1_2PI = 0.159154943091895;
+
+__constant__ const float const_1_PI_f = 0.318309886183791;
+__constant__ const float const_1_2PI_f = 0.159154943091895;
+__constant__ const double const_1_PI = 0.318309886183791;
+__constant__ const double const_1_2PI = 0.159154943091895;
+
+
 // 1/(2*pi), 2D gauss constant
 __constant__ int constmem_Nsize[5];
 // declare constant memory, Nx,Ny,Nz,N_gaussian,N_para
@@ -42,14 +42,7 @@ void set_constant_mem(int Nx, int Ny, int Nz, int N_gaussian, int N_para)
     cudaMemcpyToSymbol(constmem_Nsize, &cNsize, sizeof(int) * 5);
 }
 
-__inline__ __device__ T gauss1d(T x, T A, T mu, T sigma)
-{
-    T c{ const_1_SQRT_2PI / sigma };
-    T xnew = (x - mu) / sigma;
-    return A * c * expf(-0.5f * xnew * xnew);
-}
-
-__inline__ __device__ T gauss2d(T x, T y, T A, T mux, T muy, T sigma)
+__inline__ __device__ float gauss2d(float x, float y, float A, float mux, float muy, float sigma)
 {// isotropic 2d gaussian function
     if ((sigma < 1e-7f))
     {
@@ -57,14 +50,29 @@ __inline__ __device__ T gauss2d(T x, T y, T A, T mux, T muy, T sigma)
     }
     else
     {
-        T half_1_sigma2 = 1.0f / (2.0f * sigma * sigma);
-        T c = const_1_PI * half_1_sigma2;
-        T xnew = x - mux;
-        T ynew = y - muy;
+        float half_1_sigma2 = 1.0f / (2.0f * sigma * sigma);
+        float c = const_1_PI_f * half_1_sigma2;
+        float xnew = x - mux;
+        float ynew = y - muy;
         return A * c * expf(-half_1_sigma2 * (xnew * xnew + ynew * ynew));
     }
 }
-__inline__ __device__ T mvn2d(T x, T y, T A, T mux, T muy, T sigma1, T sigma2, T beta)
+__inline__ __device__ double gauss2d(double x, double y, double A, double mux, double muy, double sigma)
+{// isotropic 2d gaussian function
+    if ((sigma < 1e-7f))
+    {
+        return 0.0f;
+    }
+    else
+    {
+        float half_1_sigma2 = 1.0 / (2.0 * sigma * sigma);
+        float c = const_1_PI * half_1_sigma2;
+        float xnew = x - mux;
+        float ynew = y - muy;
+        return A * c * exp(-half_1_sigma2 * (xnew * xnew + ynew * ynew));
+    }
+}
+__inline__ __device__ float mvn2d(float x, float y, float A, float mux, float muy, float sigma1, float sigma2, float beta)
 {
     if ((sigma1 < 1e-7f) | (sigma2 < 1e-7f) | (A < 1e-7f))
     {
@@ -74,19 +82,43 @@ __inline__ __device__ T mvn2d(T x, T y, T A, T mux, T muy, T sigma1, T sigma2, T
     else
     {
         // Rotation matrix R, coordinate X = [x,y]-[mux,muy], Y = RX
-        T sinb = sinf(beta);
-        T cosb = cosf(beta);
-        T Y1 = cosb * (x - mux) - sinb * (y - muy);// beta is angle in rad
-        T Y2 = sinb * (x - mux) + cosb * (y - muy);
+        float sinb = sinf(beta);
+        float cosb = cosf(beta);
+        float Y1 = cosb * (x - mux) - sinb * (y - muy);// beta is angle in rad
+        float Y2 = sinb * (x - mux) + cosb * (y - muy);
         // S = Y' * M^-1 * Y
-        T S1 = (Y1 / sigma1) * (Y1 / sigma1);
-        T S2 = (Y2 / sigma2) * (Y2 / sigma2);
-        T exponant = -0.5f * (S1 + S2);
-        T scale = (A * const_1_2PI) / (sigma1 * sigma2);
+        float S1 = (Y1 / sigma1) * (Y1 / sigma1);
+        float S2 = (Y2 / sigma2) * (Y2 / sigma2);
+        float exponant = -0.5f * (S1 + S2);
+        float scale = (A * const_1_2PI_f) / (sigma1 * sigma2);
 
         return scale * expf(exponant);
     }
 }
+__inline__ __device__ double mvn2d(double x, double y, double A, double mux, double muy, double sigma1, double sigma2, double beta)
+{
+    if ((sigma1 < 1e-7) | (sigma2 < 1e-7) | (A < 1e-7))
+    {
+        // float number effective decimal digits is 7
+        return 0.0;
+    }
+    else
+    {
+        // Rotation matrix R, coordinate X = [x,y]-[mux,muy], Y = RX
+        double sinb = sin(beta);
+        double cosb = cos(beta);
+        double Y1 = cosb * (x - mux) - sinb * (y - muy);// beta is angle in rad
+        double Y2 = sinb * (x - mux) + cosb * (y - muy);
+        // S = Y' * M^-1 * Y
+        double S1 = (Y1 / sigma1) * (Y1 / sigma1);
+        double S2 = (Y2 / sigma2) * (Y2 / sigma2);
+        double exponant = -0.5 * (S1 + S2);
+        double scale = (A * const_1_2PI) / (sigma1 * sigma2);
+
+        return scale * exp(exponant);
+    }
+}
+template<class T>
 __global__ void dose3d_N_iso(T* X, T* Y, T* para, T* dose3d)
 {// isotropic 2d gaussian function
     int nx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -98,7 +130,7 @@ __global__ void dose3d_N_iso(T* X, T* Y, T* para, T* dose3d)
     int N_gaussian = constmem_Nsize[3];
     if (nz < Nz & ny < Ny & nx < Nx)
     {
-        int idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
+        int64_t idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
         T x{ X[nx] };
         T y{ Y[ny] };
         T temp{};
@@ -113,6 +145,7 @@ __global__ void dose3d_N_iso(T* X, T* Y, T* para, T* dose3d)
         dose3d[idx3d] = temp;
     }
 }
+template<class T>
 __global__ void dose3d_N_iso_Gradient(T* X, T* Y, T* para, T* output)
 {// isotropic 2d gaussian function
     int nx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -125,7 +158,7 @@ __global__ void dose3d_N_iso_Gradient(T* X, T* Y, T* para, T* output)
     if (nz < Nz & ny < Ny & nx < Nx)
     {
         int64_t N_gauss_para = int64_t(Nz) * int64_t(N_gaussian) * int64_t(4) ;
-        int idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
+        int64_t idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
         T x{ X[nx] };
         T y{ Y[ny] };
         for (int ng = 0; ng < N_gaussian; ++ng)
@@ -148,7 +181,7 @@ __global__ void dose3d_N_iso_Gradient(T* X, T* Y, T* para, T* output)
     }
 }
 
-
+template<class T>
 __global__ void dose3d_N(T* X, T* Y, T* para, T* dose3d)
 {// general 2d gaussian function
     int nx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -166,7 +199,7 @@ __global__ void dose3d_N(T* X, T* Y, T* para, T* dose3d)
     // Y_shared[ny] = Y[ny];
     // __syncthreads();
     // 
-    int idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
+    int64_t idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
     T x{ X[nx] };
     T y{ Y[ny] };
     T temp{};
@@ -183,7 +216,8 @@ __global__ void dose3d_N(T* X, T* Y, T* para, T* dose3d)
     dose3d[idx3d] = temp;
 
 }
-__global__ void dose3d_N_Gradient(T* X, T* Y, T* para, T* output)
+
+__global__ void dose3d_N_Gradient(float* X, float* Y, float* para, float* output)
 {// general 2d gaussian function
     int nx = blockIdx.x * blockDim.x + threadIdx.x;
     int ny = blockIdx.y * blockDim.y + threadIdx.y;
@@ -201,37 +235,37 @@ __global__ void dose3d_N_Gradient(T* X, T* Y, T* para, T* output)
     // __syncthreads();
     // 
     int64_t N_gauss_para = int64_t(Nz) * int64_t(N_gaussian) * int64_t(6) ;
-    int idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
-    T x{ X[nx] };
-    T y{ Y[ny] };
+    int64_t idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
+    float x{ X[nx] };
+    float y{ Y[ny] };
     for (int ng = 0; ng < N_gaussian; ++ng)
     {
-        T A = para[nz * N_gaussian * 6 + 6 * ng];
-        T mux = para[nz * N_gaussian * 6 + 6 * ng + 1];
-        T muy = para[nz * N_gaussian * 6 + 6 * ng + 2];
-        T sigma1 = para[nz * N_gaussian * 6 + 6 * ng + 3];
-        T sigma2 = para[nz * N_gaussian * 6 + 6 * ng + 4];
-        T beta = para[nz * N_gaussian * 6 + 6 * ng + 5];
-        T G = mvn2d(x, y, A, mux, muy, sigma1, sigma2, beta);
-        T sigma1_sqr = sigma1 * sigma1;
-        T sigma2_sqr = sigma2 * sigma2;
-        T One_sigma1_sqr = 1.0f / (sigma1_sqr);
-        T One_sigma2_sqr = 1.0f / (sigma2_sqr);
-        T x_mux = x - mux;
-        T y_muy = y - muy;
-        T cosb = cosf(beta);
-        T sinb = sinf(beta);
-        T Y1 = x_mux * cosb - y_muy * sinb;
-        T Y2 = x_mux * sinb + y_muy * cosb;
-        T S1 = (Y1 * Y1) / (sigma1_sqr);
-        T S2 = (Y2 * Y2) / (sigma2_sqr);
-        T Y1_sigma1_sqr = Y1 * One_sigma1_sqr;
-        T Y2_sigma2_sqr = Y2 * One_sigma2_sqr;
-        T w1 = (Y1_sigma1_sqr * cosb + Y2_sigma2_sqr * sinb);
-        T w2 = (-Y1_sigma1_sqr * sinb + Y2_sigma2_sqr * cosb);
-        T w3 = (S1 - 1.0f) / (sigma1);
-        T w4 = (S2 - 1.0f) / (sigma2);
-        T w5 = Y1 * Y2 * (One_sigma1_sqr - One_sigma2_sqr);
+        float A = para[nz * N_gaussian * 6 + 6 * ng];
+        float mux = para[nz * N_gaussian * 6 + 6 * ng + 1];
+        float muy = para[nz * N_gaussian * 6 + 6 * ng + 2];
+        float sigma1 = para[nz * N_gaussian * 6 + 6 * ng + 3];
+        float sigma2 = para[nz * N_gaussian * 6 + 6 * ng + 4];
+        float beta = para[nz * N_gaussian * 6 + 6 * ng + 5];
+        float G = mvn2d(x, y, A, mux, muy, sigma1, sigma2, beta);
+        float sigma1_sqr = sigma1 * sigma1;
+        float sigma2_sqr = sigma2 * sigma2;
+        float One_sigma1_sqr = 1.0f / (sigma1_sqr);
+        float One_sigma2_sqr = 1.0f / (sigma2_sqr);
+        float x_mux = x - mux;
+        float y_muy = y - muy;
+        float cosb = cosf(beta);
+        float sinb = sinf(beta);
+        float Y1 = x_mux * cosb - y_muy * sinb;
+        float Y2 = x_mux * sinb + y_muy * cosb;
+        float S1 = (Y1 * Y1) / (sigma1_sqr);
+        float S2 = (Y2 * Y2) / (sigma2_sqr);
+        float Y1_sigma1_sqr = Y1 * One_sigma1_sqr;
+        float Y2_sigma2_sqr = Y2 * One_sigma2_sqr;
+        float w1 = (Y1_sigma1_sqr * cosb + Y2_sigma2_sqr * sinb);
+        float w2 = (-Y1_sigma1_sqr * sinb + Y2_sigma2_sqr * cosb);
+        float w3 = (S1 - 1.0f) / (sigma1);
+        float w4 = (S2 - 1.0f) / (sigma2);
+        float w5 = Y1 * Y2 * (One_sigma1_sqr - One_sigma2_sqr);
         output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng] = G / (A);    // dG/dA
         output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng + 1] = w1 * G; // dG/dmux
         output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng + 2] = w2 * G; // dG/dmuy
@@ -241,16 +275,74 @@ __global__ void dose3d_N_Gradient(T* X, T* Y, T* para, T* output)
     }
 
 }
+__global__ void dose3d_N_Gradient(double* X, double* Y, double* para, double* output)
+{// general 2d gaussian function
+    int nx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ny = blockIdx.y * blockDim.y + threadIdx.y;
+    int nz = blockIdx.z * blockDim.z + threadIdx.z;
+    int Nx = constmem_Nsize[0];
+    int Ny = constmem_Nsize[1];
+    int Nz = constmem_Nsize[2];
+    int N_gaussian = constmem_Nsize[3];
+    // __shared__ X_shared[512];
+    // __shared__ Y_shared[512];
+    if (nx >= Nx || ny >= Ny || nz >= Nz) return;
 
-void Gauss2d::cuda_interface(std::vector<float> X, std::vector<float> Y, std::vector<float> para, float* dose3D, int Nx, int Ny, int Nz, int N_para, int N_gaussian)
+    // X_shared[nx] = X[nx];
+    // Y_shared[ny] = Y[ny];
+    // __syncthreads();
+    // 
+    int64_t N_gauss_para = int64_t(Nz) * int64_t(N_gaussian) * int64_t(6) ;
+    int64_t idx3d{ nx + ny * Nx + nz * (Nx * Ny) };
+    double x{ X[nx] };
+    double y{ Y[ny] };
+    for (int ng = 0; ng < N_gaussian; ++ng)
+    {
+        double A = para[nz * N_gaussian * 6 + 6 * ng];
+        double mux = para[nz * N_gaussian * 6 + 6 * ng + 1];
+        double muy = para[nz * N_gaussian * 6 + 6 * ng + 2];
+        double sigma1 = para[nz * N_gaussian * 6 + 6 * ng + 3];
+        double sigma2 = para[nz * N_gaussian * 6 + 6 * ng + 4];
+        double beta = para[nz * N_gaussian * 6 + 6 * ng + 5];
+        double G = mvn2d(x, y, A, mux, muy, sigma1, sigma2, beta);
+        double sigma1_sqr = sigma1 * sigma1;
+        double sigma2_sqr = sigma2 * sigma2;
+        double One_sigma1_sqr = 1.0 / (sigma1_sqr);
+        double One_sigma2_sqr = 1.0 / (sigma2_sqr);
+        double x_mux = x - mux;
+        double y_muy = y - muy;
+        double cosb = cos(beta);
+        double sinb = sin(beta);
+        double Y1 = x_mux * cosb - y_muy * sinb;
+        double Y2 = x_mux * sinb + y_muy * cosb;
+        double S1 = (Y1 * Y1) / (sigma1_sqr);
+        double S2 = (Y2 * Y2) / (sigma2_sqr);
+        double Y1_sigma1_sqr = Y1 * One_sigma1_sqr;
+        double Y2_sigma2_sqr = Y2 * One_sigma2_sqr;
+        double w1 = (Y1_sigma1_sqr * cosb + Y2_sigma2_sqr * sinb);
+        double w2 = (-Y1_sigma1_sqr * sinb + Y2_sigma2_sqr * cosb);
+        double w3 = (S1 - 1.0) / (sigma1);
+        double w4 = (S2 - 1.0) / (sigma2);
+        double w5 = Y1 * Y2 * (One_sigma1_sqr - One_sigma2_sqr);
+        output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng] = G / (A);    // dG/dA
+        output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng + 1] = w1 * G; // dG/dmux
+        output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng + 2] = w2 * G; // dG/dmuy
+        output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng + 3] = w3 * G; // dG/ds1
+        output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng + 4] = w4 * G; // dG/ds2
+        output[idx3d * N_gauss_para + nz * N_gaussian * 6 + 6 * ng + 5] = w5 * G; // dG/db
+    }
+
+}
+template<class T>
+void Gauss2d::cuda_interface(std::vector<T> X, std::vector<T> Y, std::vector<T> para, T* dose3D, int Nx, int Ny, int Nz, int N_para, int N_gaussian)
 {
     int64_t dose_size = int64_t(Nx) * int64_t(Ny) * int64_t(Nz);
     set_constant_mem(Nx, Ny, Nz, N_gaussian, N_para);
     // copy data to device
-    device_vec X_dev = X;
-    device_vec Y_dev = Y;
-    device_vec para_dev = para;
-	device_vec dose3D_dev(dose_size);
+    thrust::device_vector<T> X_dev = X;
+    thrust::device_vector<T> Y_dev = Y;
+    thrust::device_vector<T> para_dev = para;
+	thrust::device_vector<T> dose3D_dev(dose_size);
     // cast to raw pointer
     T* X_dev_ptr = thrust::raw_pointer_cast(X_dev.data());
     T* Y_dev_ptr = thrust::raw_pointer_cast(Y_dev.data());
@@ -271,17 +363,18 @@ void Gauss2d::cuda_interface(std::vector<float> X, std::vector<float> Y, std::ve
     // copy data back to host
     //thrust::copy(dose3D_dev.begin(), dose3D_dev.end(), dose3D.begin());// dose3D is std::vector or host vector
     // direct copy data to host pointer
-    cudaMemcpy(dose3D, dose3D_dev_ptr, sizeof(float)* dose_size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(dose3D, dose3D_dev_ptr, sizeof(T)* dose_size, cudaMemcpyDeviceToHost);
 }
-void Gauss2d::cuda_interface_gradient(std::vector<float> X, std::vector<float> Y, std::vector<float> para, float* grad, int Nx, int Ny, int Nz, int N_para, int N_gaussian)
+template<class T>
+void Gauss2d::cuda_interface_gradient(std::vector<T> X, std::vector<T> Y, std::vector<T> para, T* grad, int Nx, int Ny, int Nz, int N_para, int N_gaussian)
 {
     int64_t grad_size = int64_t(N_para) * int64_t(Nx) * int64_t(Ny) * int64_t(Nz);
     set_constant_mem(Nx, Ny, Nz, N_gaussian, N_para);
     // copy data to device
-    device_vec X_dev = X;
-    device_vec Y_dev = Y;
-    device_vec para_dev = para;
-	device_vec grad_dev(grad_size);
+    thrust::device_vector<T> X_dev = X;
+    thrust::device_vector<T> Y_dev = Y;
+    thrust::device_vector<T> para_dev = para;
+	thrust::device_vector<T> grad_dev(grad_size);
     // cast to raw pointer
     T* X_dev_ptr = thrust::raw_pointer_cast(X_dev.data());
     T* Y_dev_ptr = thrust::raw_pointer_cast(Y_dev.data());
@@ -300,16 +393,23 @@ void Gauss2d::cuda_interface_gradient(std::vector<float> X, std::vector<float> Y
         dose3d_N_iso << <numBlocks, threadsPerBlock >> > (X_dev_ptr, Y_dev_ptr, para_dev_ptr, grad_dev_ptr);
     }
     // copy data back to host
-    cudaMemcpy(grad, grad_dev_ptr, sizeof(float)* grad_size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(grad, grad_dev_ptr, sizeof(T)* grad_size, cudaMemcpyDeviceToHost);
 }
+
+template void Gauss2d::cuda_interface(std::vector<double> X, std::vector<double> Y, std::vector<double> para, double* dose3D, int Nx, int Ny, int Nz, int N_para, int N_gaussian);
+template void Gauss2d::cuda_interface(std::vector<float> X, std::vector<float> Y, std::vector<float> para, float* dose3D, int Nx, int Ny, int Nz, int N_para, int N_gaussian);
+template void Gauss2d::cuda_interface_gradient(std::vector<double> X, std::vector<double> Y, std::vector<double> para, double* grad, int Nx, int Ny, int Nz, int N_para, int N_gaussian);
+template void Gauss2d::cuda_interface_gradient(std::vector<float> X, std::vector<float> Y, std::vector<float> para, float* grad, int Nx, int Ny, int Nz, int N_para, int N_gaussian);
+
+
 // disable matlab entry function
 // void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-//     T *X;
-//     T *Y;
-//     T *para;
-//     X = (T*)mxGetPr(prhs[0]);
-//     Y = (T*)mxGetPr(prhs[1]);
-//     para = (T*)mxGetPr(prhs[2]);
+//     float *X;
+//     float *Y;
+//     float *para;
+//     X = (float*)mxGetPr(prhs[0]);
+//     Y = (float*)mxGetPr(prhs[1]);
+//     para = (float*)mxGetPr(prhs[2]);
 
 //     const mwSize *dim_X = mxGetDimensions(prhs[0]);
 //     const mwSize *dim_Y = mxGetDimensions(prhs[1]);
@@ -323,19 +423,19 @@ void Gauss2d::cuda_interface_gradient(std::vector<float> X, std::vector<float> Y
 
 //     set_constant_mem(Nx, Ny, Nz, N_gaussian, N_para);
 
-//     host_vec X_vec(Nx), Y_vec(Ny);
+//     thrust::host_vector<float> X_vec(Nx), Y_vec(Ny);
 //     thrust::copy(X, X + Nx, X_vec.begin());
 //     thrust::copy(Y, Y + Ny, Y_vec.begin());
 
-//     host_vec para_vec(N_para);
+//     thrust::host_vector<float> para_vec(N_para);
 //     thrust::copy(para, para + N_para, para_vec.begin());
 
 //     const mwSize size[3]{ mwSize(Nx), mwSize(Ny), mwSize(Nz) };
 //     int64_t size3d = int64_t(Nx)* int64_t(Ny)* int64_t(Nz);
 //     plhs[0] = mxCreateNumericArray(3, size, mxSINGLE_CLASS, mxREAL);
-//     T* dose3d_ptr{};
-//     dose3d_ptr = (T*)mxGetPr(plhs[0]);
-//     host_vec dose3d(size3d);
+//     float* dose3d_ptr{};
+//     dose3d_ptr = (float*)mxGetPr(plhs[0]);
+//     thrust::host_vector<float> dose3d(size3d);
 
 
 //     cpu_interface(X_vec, Y_vec, para_vec, dose3d, Nx, Ny, Nz, N_para, N_gaussian);
@@ -354,10 +454,10 @@ void Gauss2d::cuda_interface_gradient(std::vector<float> X, std::vector<float> Y
 //    int N_gaussian = 1;
 //    int N_para = 6;
 //    set_constant_mem(Nx, Ny, Nz, N_gaussian, N_para);
-//    host_vec X(Nx);
-//    host_vec Y(Ny);
-//    host_vec dose3D(Nx * Ny);
-//    host_vec para(6);// A, mux, muy, sigma1, simga2, beta(in rad)
+//    thrust::host_vector<float> X(Nx);
+//    thrust::host_vector<float> Y(Ny);
+//    thrust::host_vector<float> dose3D(Nx * Ny);
+//    thrust::host_vector<float> para(6);// A, mux, muy, sigma1, simga2, beta(in rad)
 //    para[0] = 1.0f;
 //    para[1] = 0.0f;
 //    para[2] = 0.0f;
