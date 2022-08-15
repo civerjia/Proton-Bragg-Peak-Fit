864e9a258644fa1d865daa3a8b6b0276fbf7ceb4
shuangzhou <shuangzhou@wustl.edu>
2022-08-15 00:12:31 -0500

    add auto data type detect

--- src/bf_src/BortfeldFunction.cpp
+++ src/bf_src/BortfeldFunction.cpp
@@ -1,43 +1,87 @@
 #include <cmath>
 #include "mex.h"
+#include "matrix.h"
 #include "parabolic_cylinder_function.h"
 #include "bp.h"
 #include <vector>
 
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-    float * Z{}, * para{};
-    int idx{ 1 };
-    Z = (float*)mxGetPr(prhs[0]);
-    para = (float*)mxGetPr(prhs[1]);
-    idx = int(*mxGetPr(prhs[2]));
-
+    mxClassID id0 = mxGetClassID(prhs[0]);
+    mxClassID id1 = mxGetClassID(prhs[1]);
+    // common parts
     const mwSize *dim_Z = mxGetDimensions(prhs[0]);
     const mwSize *dim_para = mxGetDimensions(prhs[1]);
     int Nz = static_cast<int>(dim_Z[0]*dim_Z[1]);
     int N_para = static_cast<int>(dim_para[0]*dim_para[1]);
 
-    if (N_para % 4 == 0)
+    if (N_para % 4 != 0)
     {
-        // check Nz 
+        mexPrintf("mod(N_para(%d),4) != 0!\n",N_para);
+        mexErrMsgTxt("Size not match!\n");
+    }
+
+    mxClassID mex_type_class = mxSINGLE_CLASS;
+    if(id0 + id1 == 2*mxDOUBLE_CLASS)
+    {
+        //mexPrintf("Double input!\n");
+        using mex_type1 = double;
+        mex_type_class = mxDOUBLE_CLASS; // mxDOUBLE_CLASS=6 mxSINGLE_CLASS=7
+
+        mex_type1 * Z{}, * para{};
+        Z = (mex_type1*)mxGetPr(prhs[0]);
+        para = (mex_type1*)mxGetPr(prhs[1]);
+        int idx = int(*mxGetPr(prhs[2]));
         if (idx == 0)
         {
             // get dose
             const mwSize size[2]{ dim_Z[0], dim_Z[1] };
-            plhs[0] = mxCreateNumericArray(2, size, mxSINGLE_CLASS, mxREAL);
-            float* idd_o_ptr;
-            idd_o_ptr = (float*)mxGetPr(plhs[0]);
+            plhs[0] = mxCreateNumericArray(2, size, mex_type_class, mxREAL);
+            mex_type1* idd_o_ptr;
+            idd_o_ptr = (mex_type1*)mxGetPr(plhs[0]);
             BP::IDD_array_N(Z, idd_o_ptr, para, Nz, N_para);
         }
         else if(idx == 1)
         {
             // get Jacobian
             const mwSize size[2]{ mwSize(Nz), mwSize(N_para) };
-            plhs[0] = mxCreateNumericArray(2, size, mxSINGLE_CLASS, mxREAL);
-            float * grad_o_ptr;
-            grad_o_ptr = (float*)mxGetPr(plhs[0]);
+            plhs[0] = mxCreateNumericArray(2, size, mex_type_class, mxREAL);
+            mex_type1 * grad_o_ptr;
+            grad_o_ptr = (mex_type1*)mxGetPr(plhs[0]);
             BP::get_jacobian(Z, grad_o_ptr, para, Nz, N_para);
         }
+    }
+    else if(id0 + id1 == 2*mxSINGLE_CLASS)
+    {
+        //mexPrintf("Single input!\n");
+        using mex_type2 = float;
+        mex_type_class = mxSINGLE_CLASS; // mxDOUBLE_CLASS=6 mxSINGLE_CLASS=7
+
+        mex_type2 * Z{}, * para{};
+        Z = (mex_type2*)mxGetPr(prhs[0]);
+        para = (mex_type2*)mxGetPr(prhs[1]);
+        int idx = int(*mxGetPr(prhs[2]));
+        if (idx == 0)
+        {
+            // get dose
+            const mwSize size[2]{ dim_Z[0], dim_Z[1] };
+            plhs[0] = mxCreateNumericArray(2, size, mex_type_class, mxREAL);
+            mex_type2* idd_o_ptr;
+            idd_o_ptr = (mex_type2*)mxGetPr(plhs[0]);
+            BP::IDD_array_N(Z, idd_o_ptr, para, Nz, N_para);
+        }
+        else if(idx == 1)
+        {
+            // get Jacobian
+            const mwSize size[2]{ mwSize(Nz), mwSize(N_para) };
+            plhs[0] = mxCreateNumericArray(2, size, mex_type_class, mxREAL);
+            mex_type2 * grad_o_ptr;
+            grad_o_ptr = (mex_type2*)mxGetPr(plhs[0]);
+            BP::get_jacobian(Z, grad_o_ptr, para, Nz, N_para);
+        }
+    }
+    else{
+        mexErrMsgTxt("Inconsistency float point input data type!, Should be all double or single\n");
     }
 
 }
